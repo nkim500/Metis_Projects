5775503c5cb80302e2db2c32d01db2caf09ee598
nkim500 <86082214+nkim500@users.noreply.github.com>
2021-12-16 17:28:04 -0500

    Update streamlit_app_2.py

--- 07_Engineering/streamlit_app_2.py
+++ 07_Engineering/streamlit_app_2.py
@@ -22,10 +22,10 @@ if choice == "Wind forecast":
     
     st.title("Find wind in your local area")
     
-    col1, col2 = st.columns([0,8])
+    col1, col2 = st.columns([0.1,8])
 
     with col1:
-        st.write("")
+        None
     with col2:
 
         url = "https://raw.githubusercontent.com/nkim500/Metis_Projects/main/07_Engineering/US_stations.csv"
